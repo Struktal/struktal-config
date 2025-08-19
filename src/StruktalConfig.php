d684148f4f96040f4f24cacff1d27a798e0ee6f5
JensOstertag <dev@jens-ostertag.de>
2025-08-19 14:02:53 +0200

    Add missing getters

--- src/StruktalConfig.php
+++ src/StruktalConfig.php
@@ -15,6 +15,10 @@ class StruktalConfig extends Config {
         return self::get("BASE_URI");
     }
 
+    public function isProduction(): bool {
+        return self::getDefault(true, "PRODUCTION");
+    }
+
     public function getDatabaseHost(): string {
         return self::get("DB_HOST");
     }
@@ -32,6 +36,46 @@ class StruktalConfig extends Config {
     }
 
     public function databaseEnabled(): bool {
-        return $this->getDefault(false, "DB_ENABLED");
+        return self::getDefault(false, "DB_ENABLED");
+    }
+
+    public function getLogRecipients(): array {
+        return self::getDefault([], "LOG_RECIPIENTS");
+    }
+
+    public function getLogLevel(): int {
+        return self::getDefault(PHP_INT_MAX, "LOG_LEVEL");
+    }
+
+    public function getSmtpHost(): string {
+        return self::get("SMTP_HOST");
+    }
+
+    public function getSmtpPort(): int {
+        return (int) self::get("SMTP_PORT");
+    }
+
+    public function getSmtpUsername(): string {
+        return self::get("SMTP_USER");
+    }
+
+    public function getSmtpPassword(): string {
+        return self::get("SMTP_PASS");
+    }
+
+    public function getSmtpSecure(): string {
+        return self::getDefault("tls", "SMTP_SECURE");
+    }
+
+    public function getSmtpAuth(): bool {
+        return self::getDefault(true, "SMTP_AUTH");
+    }
+
+    public function redirectAllMails(): bool {
+        return self::getDefault(false, "REDIRECT_ALL_MAILS");
+    }
+
+    public function getRedirectMailAddress(): string {
+        return self::getDefault("", "REDIRECT_ALL_MAILS_ADDRESS");
     }
 }
